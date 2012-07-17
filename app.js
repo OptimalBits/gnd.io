c52e5fc20afa5ebac8a4bfd35fcb7a310aa0745d
Sebastian Astudillo <sebastian@optimalbits.com>
2012-07-17 14:25:28 +0200

    Added dynamic control of content with readme.md file

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var request = require('request');
+
 var app = express.createServer();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('index.jade');
+  request('https://raw.github.com/OptimalBits/ginger/master/Readme.md',function(error,response,body){
+    if (!error && response.statusCode == 200) {
+      var output = require( "markdown" ).markdown.toHTML(body)
+      console.log(output);
+      res.render('index.jade',{content:output, moco:'pedo'});
+    }
+  })
 });
 
 app.listen(3000);
