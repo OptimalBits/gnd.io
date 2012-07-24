a5ef7cc52aa7515cf71e4a2e441d87a4931e5989
Manuel Astudillo <manuel@optimalbits.com>
2012-07-24 12:00:42 +0200

    Changed markdown module to node-markdown since the previous module was too buggy.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express'),
   request = require('request'),
-  md = require( "markdown" ),
-  app = express.createServer();
+  md = require("node-markdown").Markdown,
+  app = express.createServer(),
+  port = process.argv[2] || 8000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   request('https://raw.github.com/OptimalBits/ginger/master/Readme.md', function(error, response, body){
     if (!error && response.statusCode == 200) {
-      res.render('index.jade',{content:md.markdown.toHTML(body)});
+      res.render('index.jade', {content:md(body)});
+    }else{
+      res.send('Error generating page, please try later on...');
     }
   })
 });
 
-app.listen(process.argv[2] || 3000);
+app.listen(port);
+console.log("Started web server in port %s", port);
