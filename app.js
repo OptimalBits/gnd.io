030c2980af2f9074b5795c76515a5c100fba2f64
Sebastian Astudillo <sebastian@optimalbits.com>
2013-01-19 16:40:34 -0600

    Added chat example

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+require('require-typescript');
+
+var Gnd = require('./public/lib/ground/gnd-server.ts');
+
 var express = require('express'),
   request = require('request'),
   md = require("node-markdown").Markdown,
   app = express.createServer(),
-  port = process.argv[2] || 8000;
+  sio = require('socket.io').listen(app),
+  port = process.argv[2] || 8000,
+
+  mongoose = require('mongoose'),
+  redis = require('redis'),
+  Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
 
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
@@ -29,5 +39,62 @@ app.get('/demos/routes', function(req,res) {
   res.render('demos/routes.jade');
 })
 
-app.listen(port);
-console.log("Started web server in port %s", port);
+app.get('/demos/chat', function(req,res) {
+  res.render('demos/chat/index.jade',{ pretty: true });
+})
+/* api still not working
+app.get('/api/',function(req,res){
+  res.render('api/index.jade');
+})*/
+// Setup a mongo DB. This is used by the chat example
+var Message = new Schema({
+  _cid: {type: String},
+  text: {type: String},
+  ts: {type: Number}
+});
+
+var Room = new Schema({
+  _cid: {type: String},
+  name : {type: String},
+  url: {type: String},
+  ts: {type: Number},
+  messages :  [{type: Schema.ObjectId, ref: 'Message'}]
+});
+
+var Chat = new Schema({
+  _cid: {type: String},
+  rooms :  [{type: Schema.ObjectId, ref: 'Room'}]
+});
+
+Chat.statics.add = Room.statics.add = function(id, setName, itemIds, cb){
+  var update = {$addToSet: {}};
+  update.$addToSet[setName] = {$each:itemIds};
+  this.update({_id:id}, update, cb);
+};
+
+var models = {
+  messages: mongoose.model('Message', Message),
+  rooms: mongoose.model('Room', Room),
+  chats: mongoose.model('Chat', Chat)
+};
+
+// Setup mongodb
+mongoose.connect('mongodb://localhost/exDB', function(){
+  // mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) {
+  //   console.log(result);
+  //   mongoose.disconnect(function(){
+  //     mongoose.connect('mongodb://localhost/exDB');
+  
+      app.listen(port);
+      console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
+  //   });
+  // });
+});
+
+var mongooseStorage = new Gnd.MongooseStorage(models);
+var pubClient = redis.createClient(6379, "127.0.0.1"),
+    subClient = redis.createClient(6379, "127.0.0.1");
+
+var syncHub = new Gnd.Sync.SyncHub(pubClient, subClient, sio.sockets);
+var gndServer = new Gnd.Server(mongooseStorage, syncHub);
+var socketServer = new Gnd.SocketBackend(sio.sockets, gndServer);
