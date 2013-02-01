ce5e9191306ecd72b092ece385c3a0c94e74d76d
Manuel Astudillo <manuel@optimalbits.com>
2013-02-01 22:55:23 +0100

    included a require-typescript in app.js

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-require('require-typescript');
+//require('require-typescript');
+
+// @see http://nodejs.org/api/all.html#all_require_extensions
+var fs = require('fs')
+  , exec = require('child_process').exec
+  , uuid = require('node-uuid')
+  , fs = require('fs')
+  , execSync = require('execSync');
+
+// typescript doesn't export any public interface, we need to spawn a
+// process
+// @todo: do I need to take care of caching or is this done at a higher level??
+// @todo: cache compiled files and don't recompile them if source didn't
+//        change (last modified date comparison)
+require.extensions['.ts'] = function(module, filename) {
+  var out = 'require-typescript-' + uuid.v1() + '.js'
+  var cmd = __dirname + '/node_modules/.bin/tsc --out ' + out + ' ' + filename;
+  execSync.stdout(cmd);
+  var content = fs.readFileSync(out, 'utf8');
+  execSync.stdout('rm ' + out);
+  return module._compile(content, filename);
+};
+
+
+//
+_ = require('underscore');
 
 var Gnd = require('./public/lib/ground/gnd-server.ts');
 
