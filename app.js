1a6fb086cd4db7766619a901c179e7e4db1798ae
Manuel Astudillo <manuel@optimalbits.com>
2013-08-01 11:20:33 +0200

    Updated demos to work with latest Gnd version

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-//require('require-typescript');
+"use strict";
 
-// @see http://nodejs.org/api/all.html#all_require_extensions
-var fs = require('fs')
-  , exec = require('child_process').exec
-  , uuid = require('node-uuid')
-  , fs = require('fs')
-  , execSync = require('execSync');
-
-// typescript doesn't export any public interface, we need to spawn a
-// process
-// @todo: do I need to take care of caching or is this done at a higher level??
-// @todo: cache compiled files and don't recompile them if source didn't
-//        change (last modified date comparison)
-require.extensions['.ts'] = function(module, filename) {
-  var out = 'require-typescript-' + uuid.v1() + '.js'
-  var cmd = __dirname + '/node_modules/.bin/tsc --out ' + out + ' ' + filename;
-  execSync.stdout(cmd);
-  var content = fs.readFileSync(out, 'utf8');
-  execSync.stdout('rm ' + out);
-  return module._compile(content, filename);
-};
-
-
-//
-_ = require('underscore');
-
-var Gnd = require('./public/lib/ground/gnd-server.ts');
+var _ = require('underscore');
+var Gnd = require('gnd');
+var cabinet = require('cabinet');
 
 var express = require('express'),
-  request = require('request'),
-  md = require("node-markdown").Markdown,
-  app = express.createServer(),
-  sio = require('socket.io').listen(app),
-  port = process.env['GNDIO_PORT'] || 8000,
-  mongoHost = process.env['MONGO_HOST'] || 'localhost';
+    request = require('request'),
+    md = require("node-markdown").Markdown,
+    app = express.createServer(),
+    sio = require('socket.io').listen(app),
+    port = process.env['GNDIO_PORT'] || 8000,
+    mongoHost = process.env['MONGO_HOST'] || 'localhost',
+    mongoose = require('mongoose'),
+    redis = require('redis'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
 
-  mongoose = require('mongoose'),
-  redis = require('redis'),
-  Schema = mongoose.Schema,
-  ObjectId = Schema.ObjectId;
+//app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'));
+console.log(Gnd.lib)
+
+app.use(cabinet(__dirname + '/public', {
+  files: {
+    '/lib/gnd.js': Gnd.debug,//Gnd.lib,
+    '/lib/third/underscore.js': Gnd.third.underscore,
+    '/lib/third/curl.js': Gnd.third.curl,
+  },
+}));
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
@@ -94,12 +80,6 @@ var Chat = new Schema({
   rooms :  [{type: Schema.ObjectId, ref: 'Room'}]
 });
 
-Chat.statics.add = Room.statics.add = function(id, setName, itemIds, cb){
-  var update = {$addToSet: {}};
-  update.$addToSet[setName] = {$each:itemIds};
-  this.update({_id:id}, update, cb);
-};
-
 var models = {
   messages: mongoose.model('Message', Message),
   rooms: mongoose.model('Room', Room),
@@ -107,22 +87,19 @@ var models = {
 };
 
 // Setup mongodb
-mongoose.connect('mongodb://'+mongoHost+'/exDB', function(){
-  // mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) {
-  //   console.log(result);
-  //   mongoose.disconnect(function(){
-  //     mongoose.connect('mongodb://localhost/exDB');
-  
-      app.listen(port);
-      console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
-  //   });
-  // });
-});
+mongoose.connect('mongodb://'+mongoHost+'/gndio');
 
-var mongooseStorage = new Gnd.MongooseStorage(models);
+var mongooseStorage = new Gnd.Storage.MongooseStorage(models, mongoose, true);
 var pubClient = redis.createClient(6379, "127.0.0.1"),
     subClient = redis.createClient(6379, "127.0.0.1");
 
-var syncHub = new Gnd.Sync.SyncHub(pubClient, subClient, sio.sockets);
-var gndServer = new Gnd.Server(mongooseStorage, syncHub);
+var syncHub = new Gnd.Sync.Hub(pubClient, subClient, sio.sockets);
+var gndServer = new Gnd.Server(mongooseStorage, new Gnd.SessionManager(), syncHub);
 var socketServer = new Gnd.SocketBackend(sio.sockets, gndServer);
+
+app.listen(port);
+console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
+
+
+
+
